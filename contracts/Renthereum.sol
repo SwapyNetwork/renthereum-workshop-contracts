a5ea6472557014c752d4cdfea115020e1be1432f
Luis Philipe <luizsale10@gmail.com>
2017-11-08 19:26:18 -0200

    fix test script

--- contracts/Renthereum.sol
+++ contracts/Renthereum.sol
@@ -109,10 +109,12 @@ contract Renthereum {
     Ordered(_id, item.owner, item.name, item.dailyValue);
     return itemsCount - 1;
   }
+  
   // cancel an available order
   function cancelOrder(uint256 _index)
     isValidItem(_index, itemsForHire)
     onlyOwner(itemsForHire[_index].owner)
+    public
     returns(bool)
   {
     Order memory order = itemsForHire[_index];
--- test/Renthereum.js
+++ test/Renthereum.js
@@ -7,4 +7,37 @@ const expect = require('chai').expect;
 // --- Artifacts
 const Renthereum = artifacts.require("./Renthereum.sol");
 
-contract('Renthereum', accounts => {} )
+// --- Test variables 
+let owner = null;
+let customer = null;
+let renthereum = null;
+
+contract('Renthereum', accounts => {
+
+    before( done => {
+        owner = accounts[0];
+        customer = accounts[1];
+        Renthereum.new().then(contract => {
+            renthereum = contract;
+            done();
+        })
+    })
+    
+    it("should rent an item", done => {
+        renthereum.createOrder('123456789','bike','a great bike', 23, 1, 30).then(transaction => {
+            should.exist(transaction.tx);
+            renthereum.Ordered( { _id : '123456789' }).watch((err, log) => {
+                const event = log.args;
+                expect(event).to.include.all.keys([
+                    '_id',
+                    '_owner',
+                    '_name',
+                    '_value'
+                ]);
+                assert.equal(event._owner, owner, "The user must be the owner of the rent order");
+                done();        
+            })
+        })
+    })
+
+})
